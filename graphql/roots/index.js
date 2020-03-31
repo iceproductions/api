e2a8b7af6aef5a180c803efe1a78dcd1f1c8ec8d
Daniel Bulant <admin@danbulant.eu>
2020-03-31 17:28:36 +0200

    Implement command list

--- graphql/roots/index.js
+++ graphql/roots/index.js
@@ -1,8 +1,10 @@
 const ServerInfo = require("./dataHolders/ServerInfo");
 const client = require("./mysql");
+const CommandList = require("./dataHolders/CommandList");
 
 module.exports = {
     info: () => {
         return new ServerInfo(client);
-    }
+    },
+    commands: CommandList
 };
