5e2ae2a17adbd91ad47b857d7b60188366c8cf6f
Daniel Bulant <admin@danbulant.eu>
2020-03-31 16:27:27 +0200

    Fix formatting

--- graphql/roots/dataHolders/MysqlTable.js
+++ graphql/roots/dataHolders/MysqlTable.js
@@ -1,13 +1,13 @@
 module.exports = class MysqlTable {
-    constructor (client, tableName) {
+    constructor(client, tableName) {
         this.client = client;
         this.tableName = tableName;
     }
 
-    fetchAll () {
+    fetchAll() {
         return new Promise((resolve, reject) => {
             this.getFields(["*"]).then(fields => {
-                for (var field in fields) {
+                for(var field in fields) {
                     this["_" + field] = fields[field];
                 }
                 resolve();
@@ -15,12 +15,12 @@ module.exports = class MysqlTable {
         });
     }
 
-    getFields (fields) {
+    getFields(fields) {
         return new Promise((resolve, reject) => {
             console.log("[QUERY]", fields.join());
 
             var query = "SELECT ";
-            for (var field of fields) {
+            for(var field of fields) {
                 query += "`" + field + "`, ";
             }
             query = query.substr(0, query.length - 2);
@@ -29,9 +29,9 @@ module.exports = class MysqlTable {
             // console.log(query);
 
             this.client.query(query, [this._id], (err, resp) => {
-                if (err) return reject(err);
-                if (!resp[0]) return resolve(null);
-                for (var field in resp[0]) {
+                if(err) return reject(err);
+                if(!resp[0]) return resolve(null);
+                for(var field in resp[0]) {
                     this["_" + field] = resp[0][field];
                 }
                 resolve(resp[0]);
@@ -39,20 +39,20 @@ module.exports = class MysqlTable {
         });
     }
 
-    query (query, args) {
+    query(query, args) {
         return new Promise((resolve, reject) => {
             this.client.query(query, args, (err, resp) => {
-                if (err) return reject(err);
+                if(err) return reject(err);
                 resolve(resp);
             });
         });
     }
 
-    async getField (field) {
-        if (this["_" + field]) return this["_" + field];
+    async getField(field) {
+        if(this["_" + field]) return this["_" + field];
 
         var fields = await this.getFields([field]);
-        if (!fields) return null;
+        if(!fields) return null;
         return fields[field];
     }
 };
