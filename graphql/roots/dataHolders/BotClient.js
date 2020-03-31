52b9812ded84d7549f24d0a4f8019ee6ad4ef2d4
Daniel Bulant <admin@danbulant.eu>
2020-03-31 18:21:42 +0200

    Add timeout

--- graphql/roots/dataHolders/BotClient.js
+++ graphql/roots/dataHolders/BotClient.js
@@ -3,7 +3,12 @@ const got = require("got");
 class BotClient {
     async status() {
         try {
-            await got("http://localhost:8856/");
+            await got("http://localhost:8856/", {
+                timeout: {
+                    lookup: 50,
+                    socket: 200
+                }
+            });
             return 1;
         } catch(e) {
             console.warn(e);
