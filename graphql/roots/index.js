f742251ea278779623130a04893ce67d2ea2375f
Daniel Bulant <admin@danbulant.eu>
2020-04-21 12:34:16 +0200

    Implement guild

--- graphql/roots/index.js
+++ graphql/roots/index.js
@@ -1,10 +1,16 @@
 const ServerInfo = require("./dataHolders/ServerInfo");
 const client = require("./mysql");
 const CommandList = require("./dataHolders/CommandList");
+const Guild = require("./dataHolders/Guild");
 
 module.exports = {
     info: () => {
         return new ServerInfo(client);
+    },
+    guild: async ({ id }) => {
+        var guild = new Guild(id);
+        if((await guild.fetchGuild()).error) return null;
+        return guild;
     },
     commands: CommandList,
     command: ({ name }) => {
