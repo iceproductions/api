e183c32bf0a50a4f3510ba4c7c5016bb2c374613
Daniel Bulant <admin@danbulant.eu>
2020-04-24 09:51:28 +0200

    Make paypal optional

--- index.js
+++ index.js
@@ -14,9 +14,11 @@ const root = require("./graphql/roots/");
 
 const config = require("./config.json");
 
-const gateway = braintree.connect({
-    accessToken: config.paypal.access
-});
+if(config.paypal){
+    const gateway = braintree.connect({
+        accessToken: config.paypal.access
+    });
+}
 
 const PORT = 8855;
 
@@ -40,43 +42,45 @@ app.get("/version", (req, res) => {
     });
 });
 
-app.get("/client_token", (req, res) => {
-    gateway.clientToken.generate({}, function(err, response) {
-        if(err)throw err;
-        res.send(response.clientToken);
+if(config.paypal){
+    app.get("/client_token", (req, res) => {
+        gateway.clientToken.generate({}, function(err, response) {
+            if(err)throw err;
+            res.send(response.clientToken);
+        });
     });
-});
 
-app.post("/nonce", (req, res) => {
-    console.log("Making sale");
-    var saleRequest = {
-        amount: req.body.amount,
-        merchantAccountId: "USD",
-        paymentMethodNonce: req.body.nonceV,
-        orderId: req.body.orderId,
-        descriptor: {
-            name: "ice bot*Donation"
-        },
-        options: {
-            paypal: {
-                customField: "PayPal custom field",
-                description: "Testing payment"
+    app.post("/nonce", (req, res) => {
+        console.log("Making sale");
+        var saleRequest = {
+            amount: req.body.amount,
+            merchantAccountId: "USD",
+            paymentMethodNonce: req.body.nonceV,
+            orderId: req.body.orderId,
+            descriptor: {
+                name: "ice bot*Donation"
             },
-            submitForSettlement: true
-        }
-    };
+            options: {
+                paypal: {
+                    customField: "PayPal custom field",
+                    description: "Testing payment"
+                },
+                submitForSettlement: true
+            }
+        };
 
-    gateway.transaction.sale(saleRequest, function(err, result) {
-        if(err) {
-            res.send("<h1>Error:  " + err + "</h1>");
-        } else if(result.success) {
-            res.send("<h1>Success! Transaction ID: " + result.transaction.id + "</h1>");
-        } else {
-            res.send("<h1>Error happened:  " + result.message + "</h1>");
-        }
+        gateway.transaction.sale(saleRequest, function(err, result) {
+            if(err) {
+                res.send("<h1>Error:  " + err + "</h1>");
+            } else if(result.success) {
+                res.send("<h1>Success! Transaction ID: " + result.transaction.id + "</h1>");
+            } else {
+                res.send("<h1>Error happened:  " + result.message + "</h1>");
+            }
+        });
+        console.log("Done sale");
     });
-    console.log("Done sale");
-});
+}
 
 app.listen(PORT, () => {
     console.log("Ready");
